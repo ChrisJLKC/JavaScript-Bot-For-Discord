48585e9d5a09b4c7ba149259f3116b4e40ec8cd9
Christopher Jenner <49998052+ChrisJLKC@users.noreply.github.com>
2020-07-26 12:41:39 +0100

    REMOVED TOKEN

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = 'NzM2ODg4NjQ2NzE1NzAzNDA4.Xx1XCA.RfIEJJWd4xXxm1enELBt4ZObJI0';
+const token = 'TOKEN';
 
 client.on('Ready', () =>{
     console.log('Ready!');
